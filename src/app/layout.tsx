3641f19c37dc3d9ba6d722b04983d9926afcbcf5
Karasowl <adventismael@gmail.com>
2025-05-07 13:32:03 -0600

    Agregar script de Metricool para seguimiento de métricas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter, Poppins, Kanit, Plus_Jakarta_Sans, Montserrat } from "next/font/google"
 import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script"
 //import TransitionLayout from "@/components/Desktop/TransitionLayout";
 
 const inter = Inter({
@@ -57,6 +58,23 @@ export default function RootLayout({
             {children}
             <Analytics />
         {/* </TransitionLayout> */}
+        {/* Script de Metricool */}
+        <Script id="metricool" strategy="afterInteractive">
+          {`
+            function loadScript(a){
+              var b=document.getElementsByTagName("head")[0],
+                  c=document.createElement("script");
+              c.type="text/javascript";
+              c.src="https://tracker.metricool.com/resources/be.js";
+              c.onreadystatechange=a;
+              c.onload=a;
+              b.appendChild(c)
+            }
+            loadScript(function(){
+              beTracker.t({hash:"3e527613498a2c475d56b59c0b9e897a"})
+            });
+          `}
+        </Script>
       </body>
     </html>
   );
